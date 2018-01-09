285975d18527fe737a5cbd32f4296cf84d8b5d7c
hirohiro <hirohiro_j3cub@yahoo.co.jp>
2018-01-10 03:05:49 +0900

    ore no hatu commit!

---
+++ Kemo/DataIO/DataFileManager.cs
@@ -0,0 +1,301 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Kemo.DataIO
+{
+    /*
+     
+        ToDo
+        
+
+    */
+
+    /// <summary>
+    /// ファイルの読み込み結果
+    /// </summary>
+    public enum LoadState
+    {
+        /// <summary>
+        /// ファイルが存在しなかった
+        /// </summary>
+        FileNotFound,
+        /// <summary>
+        /// ファイルをデータに変換できなかった
+        /// </summary>
+        FileNotConverted,
+        /// <summary>
+        /// データのに異常があった
+        /// </summary>
+        ConsistencyError,
+        /// <summary>
+        /// ロードに成功した
+        /// </summary>
+        Complete,
+        /// <summary>
+        /// エラー確認をしなかった、又は他の結果以外の状態
+        /// </summary>
+        None
+    }
+
+    /// <summary>
+    /// オブジェクトの整合性を確認するメソッドを定義します。
+    /// </summary>
+    interface IConsistencyCheck
+    {
+        bool ConsistencyCheck();
+    }
+
+    /// <summary>
+    /// 
+    /// <para>
+    /// データの読み書き・および暗号化を簡単に行えるクラスです。
+    /// データは一つのファイルとして保存されます。
+    /// </para>
+    /// 
+    /// <para>
+    /// 複数のデータを扱いたい場合は、複数の<see cref="DataFileManager{Type}"/>を使用するか、
+    /// AllDataFileManagerを使ってくださ（未実装）
+    /// </para>
+    /// 
+    /// </summary>
+    /// <typeparam name="Type">保存・読み込みするデータの型</typeparam>
+    public class DataFileManager<Type>
+    {
+        readonly byte[] cryptoTag = Encoding.UTF8.GetBytes("[ISCRYPTO]");
+        byte[] key;
+        /// <summary>
+        /// ファイルを保存するパスを表します。
+        /// </summary>
+        public string FilePath { get; }
+        /// <summary>
+        /// このマネージャーがファイルを読み書きするとき、暗号化・復号をするかを表します。
+        /// </summary>
+        public bool IsCrypto { get; }
+        /// <summary>
+        /// このマネージャーがオブジェクトをファイル化する時のシリアライズ法を表します。
+        /// </summary>
+        public SerializeType SerializeType { get { return serializer.SerializeType; } }
+
+        private Serializer serializer { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath">ファイルを保存するパス</param>
+        /// <param name="serializeType">ファイル化に使用するシリアライズ法</param>
+        public DataFileManager(string filePath, SerializeType serializeType = SerializeType.XML)
+        {
+            serializer = new Serializer(SerializeType.XML);
+
+            FilePath = filePath;
+            IsCrypto = false;
+
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath">ファイルを保存するパス</param>
+        /// <param name="key">暗号化・復号に使用するパスワード</param>
+        /// <param name="isCrypto">暗号化・復号のON・OFF（trueでON）</param>
+        /// <param name="serializeType">ファイル化に使用するシリアライズ法</param>
+        public DataFileManager(string filePath, string key, bool isCrypto = true, SerializeType serializeType = SerializeType.XML)
+            : this(filePath, serializeType)
+        {
+            IsCrypto = isCrypto;
+
+            List<byte> keyB = Encoding.UTF8.GetBytes(key).ToList();
+            while (keyB.Count < 24)
+            {
+                keyB.AddRange(keyB);
+            }
+
+            this.key = keyB.Take(24).ToArray();
+        }
+
+        /// <summary>
+        /// <paramref name="saveObject"/>を<see cref="FilePath"/>にセーブします。
+        /// </summary>
+        /// <param name="saveObject"></param>
+        public void Save(Type saveObject)
+        {
+            Save(saveObject, IsCrypto);
+        }
+
+        /// <summary>
+        /// 暗号化がOFFで、かつファイルが暗号化されている場合は暗号化を解きます。
+        /// ただし、コンストラクターでパスワードが指定されていないときは動作しません。
+        /// </summary>
+        public void FileDecrypto()
+        {
+            Type save;
+            Load(true, out save, false);
+            Save(save);
+        }
+
+        /// <summary>
+        /// エラーチェック無しのロード。ロードしたデータを返します。
+        /// 読み込めなかった場合、エラーが発生します。
+        /// </summary>
+        /// <returns></returns>
+        public Type Load()
+        {
+            Type ret;
+            Load(IsCrypto, out ret, false);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// エラーをチェックし、<paramref name="loadData"/>にファイルをロードします。エラーチェックの結果を<see cref="LoadState"/>で返します。
+        /// </summary>
+        /// <param name="loadData">データを格納する変数</param>
+        /// <returns></returns>
+        public LoadState TryLoad(out Type loadData)
+        {
+            return Load(IsCrypto, out loadData, true);
+        }
+
+        protected bool FileIsCrypto()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return false;
+            }
+
+            FileStream fileStream = File.Open(
+                FilePath,
+                FileMode.Open
+            );
+
+            byte[] streamTag = new byte[cryptoTag.Length];
+
+            fileStream.Read(streamTag, 0, cryptoTag.Length);
+
+            fileStream.Close();
+
+            return cryptoTag.SequenceEqual(streamTag);
+        }
+
+        protected void Save(Type saveObject, bool crypto)
+        {
+            // 書き込むファイルを確保
+            FileStream fileStream = File.Open(
+                FilePath,
+                FileMode.Create
+            );
+
+            if (crypto)
+            {
+                byte[] iv = SymmetricAlgorithm.Create().IV;
+
+                // ファイルへの暗号化込みのストリーム
+                CryptoStream cryptoStream = new CryptoStream(
+                    fileStream,
+                    new TripleDESCryptoServiceProvider().CreateEncryptor(key, iv),
+                    CryptoStreamMode.Write
+                );
+
+                //暗号化タグとIVを挿入
+                fileStream.Write(cryptoTag, 0, cryptoTag.Length);
+                fileStream.Write(iv, 0, 8);
+
+                // saveObjectをシリアルにして、暗号化ストリームに書き込み
+                serializer.Serialize(cryptoStream, saveObject);
+                // 暗号化ストリームくろーず
+                cryptoStream.Close();
+            }
+            else
+            {
+                // saveObjectをシリアルにして、ストリームに書き込み
+                serializer.Serialize(fileStream, saveObject);
+            }
+            // くろーず
+            fileStream.Close();
+        }
+
+        protected LoadState Load(bool crypto, out Type loadData, bool tryLoad)
+        {
+            if (tryLoad && !File.Exists(FilePath))
+            {
+                loadData = default(Type);
+                return LoadState.FileNotFound;
+            }
+
+            LoadState returnState = LoadState.None;
+
+            // 読み込むファイルを確保
+            using (FileStream fileStream = File.Open(FilePath, FileMode.Open))
+            {
+                if (crypto)
+                {
+                    fileStream.Seek(cryptoTag.Length, SeekOrigin.Begin);
+                    CryptoStream cryptoStream;
+                    byte[] iv = new byte[8];
+
+                    fileStream.Read(iv, 0, 8);
+
+                    // 復号込みのストリーム
+                    cryptoStream = new CryptoStream(
+                        fileStream,
+                        new TripleDESCryptoServiceProvider().CreateDecryptor(key, iv),
+                        CryptoStreamMode.Read
+                    );
+
+                    if (tryLoad)
+                    {
+                        if (serializer.TryDeserialize(cryptoStream, out loadData))
+                        {
+                            returnState = LoadState.Complete;
+                        }
+                        else
+                        {
+                            returnState = LoadState.FileNotConverted;
+                        }
+                    }
+                    else
+                    {
+                        loadData = serializer.Deserialize<Type>(cryptoStream);
+                    }
+                    // くろーず
+                    cryptoStream.Close();
+                }
+                else
+                {
+                    // ストリームから読み込み。Typeオブジェクトへ変換
+
+                    if (tryLoad)
+                    {
+                        if (serializer.TryDeserialize(fileStream, out loadData))
+                        {
+                            returnState = LoadState.Complete;
+                        }
+                        else
+                        {
+                            returnState = LoadState.FileNotConverted;
+                        }
+                    }
+                    else
+                    {
+                        loadData = serializer.Deserialize<Type>(fileStream);
+                    }
+                }
+            }
+
+            if (returnState == LoadState.Complete && loadData is IConsistencyCheck cObj && !cObj.ConsistencyCheck())
+            {
+                return LoadState.ConsistencyError;
+            }
+
+            return returnState;
+        }
+
+    }
+
+
+
+}
---
+++ Kemo/DataIO/Serializer.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Kemo.DataIO
+{
+    /*
+     
+         ToDo
+          
+          Binary方式を実装
+         
+         
+         */
+
+    /// <summary>
+    /// <see cref="Serializer{Type}"/>が使用するシリアライズ法を表します。
+    /// </summary>
+    public enum SerializeType
+    {
+        XML,
+        Binary
+    }
+
+    /// <summary>
+    /// 指定の方法で色々な型のobjectと<see cref="Stream"/>の内容を相互変換するクラスです。
+    /// </summary>
+    /// <typeparam name="Type"></typeparam>
+    public class Serializer
+    {
+        /// <summary>
+        /// <paramref name="serializeType"/>で指定した方法で色々な型のobjectと<see cref="Stream"/>の内容を相互変換します。
+        /// </summary>
+        /// <param name="serializeType">使用するシリアライズ方法</param>
+        public Serializer(SerializeType serializeType)
+        {
+            this.SerializeType = serializeType;
+        }
+
+        /// <summary>
+        /// 使用するシリアライズ方法
+        /// </summary>
+        public SerializeType SerializeType { get; }
+
+        /// <summary>
+        /// <paramref name="stream"/>を<see cref="SerializeType"/>方式で<typeparamref name="Type"/>型に変換して<paramref name="returnObj"/>に代入します。
+        /// 成功した場合trueを、そうでない場合falseを返します。
+        /// </summary>
+        /// <typeparam name="Type">取得するオブジェクトの型</typeparam>
+        /// <param name="stream">入力ストリーム</param>
+        /// <param name="returnObj">取得したオブジェクトを代入する変数</param>
+        /// <returns></returns>
+        public bool TryDeserialize<Type>(Stream stream, out Type returnObj)
+        {
+            try
+            {
+                returnObj = Deserialize<Type>(stream);
+            }
+            catch (System.InvalidOperationException ex )
+            {
+                if (ex.InnerException is XmlException)
+                {
+                    returnObj = default(Type);
+                    return false;
+                }
+
+                throw;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// <paramref name="stream"/>を<see cref="SerializeType"/>方式で<typeparamref name="Type"/>型に変換して返します
+        /// 読み込めなかった場合、エラーが発生します。
+        /// </summary>
+        /// <typeparam name="Type">取得するオブジェクトの型</typeparam>
+        /// <param name="stream">入力ストリーム</param>
+        /// <returns></returns>
+        public Type Deserialize<Type>(Stream stream)
+        {
+            Type ret = default(Type);
+
+            switch (SerializeType)
+            {
+                case SerializeType.XML:
+                    // XMLとしてストリームから読み込み。Typeオブジェクトへ変換
+                    XmlSerializer serializer = new XmlSerializer(typeof(Type));
+                    ret = (Type)serializer.Deserialize(stream);
+                    break;
+
+                case SerializeType.Binary:
+                    throw new System.NotImplementedException();
+
+                default:
+                    throw new System.NotImplementedException();
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// <paramref name="typeObj"/>を<see cref="SerializeType"/>方式で変換し、<paramref name="stream"/>に書き込みます。
+        /// </summary>
+        /// <typeparam name="Type">変換元のオブジェクトの型</typeparam>
+        /// <param name="stream">出力ストリーム</param>
+        /// <param name="typeObj">変換するデータ</param>
+        public void Serialize<Type>(Stream stream, Type typeObj)
+        {
+            try
+            {
+                switch (SerializeType)
+                {
+                    case SerializeType.XML:
+
+                        // saveObjectをXMLにして、ストリームに書き込み
+                        XmlSerializer serializer = new XmlSerializer(typeof(Type));
+                        serializer.Serialize(stream, typeObj);
+                        break;
+
+                    case SerializeType.Binary:
+
+                        throw new System.NotImplementedException();
+
+                    default:
+
+                        throw new System.NotImplementedException();
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
