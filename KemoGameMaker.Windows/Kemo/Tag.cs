66bd243ee5ffc1c7b2997484c45be64cc5c5cc5e
hirohiro <hirohiro_j3cub@yahoo.co.jp>
2018-01-17 20:17:18 +0900

    さまざまな用途で用いるタグのクラス

---
+++ KemoGameMaker.Windows/Kemo/Tag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kemo
+{
+    public class Tag :IEqualityComparer
+    {
+        public Tag(string tagName)
+        {
+            if(tagName.Length == 0)
+            {
+                throw new ArgumentException(nameof(tagName)+"の長さは１以上である必要があります。");
+            }
+            TagName = tagName ?? throw new ArgumentNullException(nameof(tagName));
+        }
+
+        public string TagName { get; }
+
+        public new bool Equals(object x, object y)
+        {
+            if(x == null || y == null)
+            {
+                return x == y;
+            }
+
+            return ((Tag)x).TagName == ((Tag)y).TagName;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            if (obj == null)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
